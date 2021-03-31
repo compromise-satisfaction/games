abe6732b5c5d8e76b73ad31977c6717df7184d73
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2021-03-31 23:37:18 +0900

    a

--- js/main.js
+++ js/main.js
@@ -115,7 +115,7 @@ function Game_load(width,height){
         Button[i]._element.style.borderRadius = "0%";
         Button[i]._element.style.webkitAppearance = "none";
         Button[i].backgroundColor = "buttonface";
-        console.log(Button[i]._element.style);
+        //console.log(Button[i]._element.style);
         Button[i]._element.onclick = function(e){
           switch(i){
             case 0:
